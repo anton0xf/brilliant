8ed993a3772e56689074531216413dd96dd1d3dc
anton0xf <anton0xf@gmail.com>
2023-05-08 15:01:01 +0300

    geometry ex/quil: fix project description and licence

--- geometry/2d-examples/quil-sketch/project.clj
+++ geometry/2d-examples/quil-sketch/project.clj
@@ -1,7 +1,6 @@
 (defproject quil-sketch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "quil usage example"
+  :url "https://github.com/anton0xf/brilliant"
+  :license {:name "public domain" :url "https://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]])
