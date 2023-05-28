23f3a115ec26b6ffb01f2fcc23ff88b546e30536
anton0xf <anton0xf@gmail.com>
2023-05-28 15:21:14 +0300

    geometry ex/swing: some code from tutor https://gist.github.com/daveray/1441520

--- geometry/2d-examples/swing-drag/src/swing_drag/core.clj
+++ geometry/2d-examples/swing-drag/src/swing_drag/core.clj
@@ -1,7 +1,31 @@
 (ns swing-drag.core
-  (:gen-class))
+  (:gen-class)
+  (:require [seesaw.core :refer :all]))
+
+(def f (frame :title "Get to know Seesaw"))
+(def lbl (label "I'm another label"))
+(defn display [content]
+  (config! f :content content)
+  content)
+(def b (button :text "Click Me"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; example from https://gist.github.com/daveray/1441520
+  (native!)
+  (-> f pack! show!)
+  (config f :title)
+  (config! f :title "No RLY, get to know Seesaw!")
+  (config! f :content "This is some content")
+  (config! f :content lbl)
+  (pack! f)
+  (display lbl)
+  (config! lbl :background :pink :foreground "#00f")
+  (config! lbl :font "ARIAL-BOLD-21")
+  (alert "I'm an alert")
+  (input "What's your favorite color?")
+  ;; => "blue"
+  (display b)
+  (listen b :action (fn [e] (alert e "Thanks!")))
+  ;; (*1) ;; unregister action
+  )
