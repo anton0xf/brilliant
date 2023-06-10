1b4e26476450ef42e85a849301ae87b1767dab85
anton0xf <anton0xf@gmail.com>
2023-06-10 10:37:37 +0300

    geometry ex/swing: draw a red cross

--- geometry/2d-examples/swing-drag/src/swing_drag/core.clj
+++ geometry/2d-examples/swing-drag/src/swing_drag/core.clj
@@ -1,25 +1,44 @@
 (ns swing-drag.core
   (:gen-class)
   (:require [seesaw.core :refer :all]
+            [seesaw.graphics :as g2]
             [swing-drag.util :refer :all]))
 
 (native!)
 
-(def scene (canvas))
+(defn paint-scene [c g]
+  (let [w (.getWidth c)
+        h (.getHeight c)
+        line-style (g2/style :foreground java.awt.Color/RED
+                             :stroke (g2/stroke :width 5))]
+    (g2/push
+     g (doto g
+         ;; (g2/anti-alias)
+         ;; (.setRenderingHint java.awt.RenderingHints/KEY_ANTIALIASING
+         ;;                    java.awt.RenderingHints/VALUE_ANTIALIAS_OFF)
+         (draw-styled
+          line-style
+          (g2/line 0 0 w h)
+          (g2/line w 0 0 h))))))
+
+(def scene (canvas :paint #(paint-scene %1 %2)))
+(def status-panel (label "Status panel"))
 
 (def main-pane (border-panel :center scene
-                             :south "Status panel"))
+                             :south status-panel))
 
 (def main-frame
   (frame :title "Dragging square"
-         :on-close :exit
+         :on-close :dispose ;; :exit
          :size [300 :by 100]
          :content main-pane))
 
 (comment
   (config! main-frame :content main-pane)
-  (config! main-pane :center scene)
-  (.repaint main-pane)
+  (first (config main-pane :south))
+  (config! status-panel :text "Status!")
+  (config! scene :paint paint-scene)
+  (.repaint scene)
   )
 
 (defn -main [& args]
